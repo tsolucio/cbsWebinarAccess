467b28ff9a8df99441fdadf3be2a771f1f91a8a0
= <=>
2014-11-27 16:38:59 +0100

    Webinar Access

---
+++ modules/cbsWebinarAccess/language/en_us.lang.php
@@ -0,0 +1,27 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$mod_strings = Array(
+    'cbsWebinarAccess' => 'Webinar Access',
+    'cbsWebinarAccess' => 'Webinar Access',
+    'webinaraccessno' => 'Webinar Access No.',
+    'access_date' => 'Access Date',
+    'access_time' => 'Access Time',
+    'relid' => 'Related to',
+    'access_url' => 'Access URL',
+    'srvprovidor' => 'SRV Providor',
+    'attended' => 'Attended',
+
+    'LBL_CUSTOM_INFORMATION' => 'Custom Information',
+    'LBL_CBSWEBINARACCESS_INFORMATION' => 'Webinar Access Information',
+    'LBL_DESCRIPTION_INFORMATION' => 'Description',
+);
+
+?>
